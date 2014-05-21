0ecb165537ec4a6207294e11245a272e8ffc0105
Jan Verley <jan.verley@siemens.com>
2014-05-21 14:05:09 +0200

    basic vm structure

---
+++ ListManager/ListManager/ViewModel/Item.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ListManager.ViewModel
+{
+  class Item : INotifyPropertyChanged
+  {
+    private string name;
+
+    public string Name
+    {
+      get { return name; }
+      set
+      {
+        if (!Equals(name, value))
+        {
+          name = value;
+          PropertyChanged(this, new PropertyChangedEventArgs("Name"));
+        }
+      }
+    }
+
+    private bool isFavorite;
+
+    public bool IsFavorite
+    {
+      get { return isFavorite; }
+      set
+      {
+        if (!Equals(isFavorite, value))
+        {
+          isFavorite = value;
+          PropertyChanged(this, new PropertyChangedEventArgs("IsFavorite"));
+        }
+      }
+    }
+
+    private bool isCurrent;
+
+    public bool IsCurrent
+    {
+      get { return isCurrent; }
+      set
+      {
+        if (!Equals(isCurrent, value))
+        {
+          isCurrent = value;
+          PropertyChanged(this, new PropertyChangedEventArgs("IsCurrent"));
+        }
+      }
+    }
+
+    public ICommand SaveCommand { get; private set; }
+    public ICommand RenameCommand { get; private set; }
+    public ICommand DeleteCommand { get; private set; }
+
+    public event PropertyChangedEventHandler PropertyChanged = delegate { };
+  }
+}
---
+++ ListManager/ListManager/ViewModel/List.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListManager.ViewModel
+{
+  class List : INotifyCollectionChanged, INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    public event NotifyCollectionChangedEventHandler CollectionChanged = delegate { };
+  }
+}
