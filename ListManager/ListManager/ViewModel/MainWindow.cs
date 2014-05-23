cb15d391a3502004014816891fa658e6a8794184
Jan Verley <jan.verley@siemens.com>
2014-05-23 18:03:49 +0200

    implement a few examples

--- ListManager/ListManager/ViewModel/MainWindow.cs
+++ ListManager/ListManager/ViewModel/MainWindow.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.ObjectModel;
+﻿using Microsoft.Practices.Prism.Commands;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Windows;
 
 namespace ListManager.ViewModel
 {
@@ -7,9 +9,31 @@ namespace ListManager.ViewModel
   {
     public MainWindow()
     {
+      var renameItem = new Item("Rename Me!", true);
+
+      renameItem.Name.AcceptNewNameCmd = new DelegateCommand<string>((newName) =>
+      {
+        if (MessageBox
+          .Show(string.Format("Rename requested!\n Accept?\nNew Name: {0}",newName), "Confirm", MessageBoxButton.YesNo)
+          .Equals(MessageBoxResult.Yes))
+        {
+          renameItem.Name.Name = newName;
+          renameItem.Name.DefaultEditText = newName;
+        }
+        });
+
       items = new ObservableCollection<Item>{
         new Item("Default", false){CanDelete = false},
         new Item("Item1", true),
+        new Item("Save Me!", true, () => 
+          {
+            return MessageBox
+              .Show("Save requested!\n Accept?","Confirm",MessageBoxButton.YesNo)
+              .Equals(MessageBoxResult.Yes);
+          }),
+        new Item("UnRenamable", false),
+        renameItem,
+
         new Item("Item1", true),
       };
     }
